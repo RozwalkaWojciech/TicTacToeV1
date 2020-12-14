3724b542543989f7eefa3c3053725d3ca59cdfc5
Wojciech Rozwałka <wojciechrozwalka@gmail.com>
2020-12-14 23:12:19 +0100

    start Menu class

--- src/main/java/game/java/game/Game.java
+++ src/main/java/game/java/game/Game.java
@@ -1,10 +1,13 @@
 package game.java.game;
 
+import game.java.menu.Menu;
+
 public class Game {
 
     private char x;
     private char o;
     private char[][] grid;
+    private char win;
 
     public Game() {
         x = 'x';
@@ -12,4 +15,7 @@ public class Game {
         grid = new char[5][5];
     }
 
+    public void start(Game game){
+        Menu.start(game);
+    }
 }
---
+++ src/main/java/game/java/menu/Menu.java
@@ -0,0 +1,18 @@
+package game.java.menu;
+
+import game.java.game.Game;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public final class Menu {
+
+    private static final Logger STDOUT = LoggerFactory.getLogger("CONSOLE_OUT");
+
+    private Menu() {
+    }
+
+    public static void start(Game game) {
+
+    }
+
+}
