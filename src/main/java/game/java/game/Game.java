ed7a0c5829f66177273753c82073e35ce4a7b3f5
Wojciech Rozwałka <wojciechrozwalka@gmail.com>
2020-12-14 23:05:12 +0100

    start game class

---
+++ src/main/java/game/java/game/Game.java
@@ -0,0 +1,15 @@
+package game.java.game;
+
+public class Game {
+
+    private char x;
+    private char o;
+    private char[][] grid;
+
+    public Game() {
+        x = 'x';
+        o = 'o';
+        grid = new char[5][5];
+    }
+
+}
