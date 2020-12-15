02613d70edc8f2db21844a40ab24f259430ce071
Wojciech Rozwałka <wojciechrozwalka@gmail.com>
2020-12-15 21:18:29 +0100

    print grid complete

--- src/main/java/game/java/game/Game.java
+++ src/main/java/game/java/game/Game.java
@@ -2,17 +2,21 @@ package game.java.game;
 
 import game.java.menu.Menu;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Game {
 
     private char x;
     private char o;
-    private char[][] grid;
+    private Character[][] grid;
     private char win;
 
     public Game() {
         x = 'x';
         o = 'o';
-        grid = new char[3][3];
+        grid = new Character[3][3];
     }
 
     public void start(Game game) {
@@ -32,6 +36,11 @@ public class Game {
     private void printGrid() {
         for (int i = 0; i < 3; i++) {
             System.out.println();
+
+            if (i != 0) {
+                System.out.println("-----");
+            }
+
             for (int j = 0; j < 3; j++) {
                 System.out.print(grid[i][j]);
                 if (j != 2) {
