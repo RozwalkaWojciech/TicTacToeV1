c3d6e494a101f6d70e2c003ed04edb17236cea76
Wojciech Rozwałka <wojciechrozwalka@gmail.com>
2020-12-16 11:27:52 +0100

    add check field is empty

--- src/main/java/game/java/game/Game.java
+++ src/main/java/game/java/game/Game.java
@@ -29,7 +29,11 @@ public class Game {
             Menu.printGrid(game);
             playerMove();
         }
-        Menu.winMenu(win);
+        if (win == 't') {
+            Menu.tieMenu();
+        } else {
+            Menu.winMenu(win);
+        }
     }
 
     private void initEmptyGrid() {
@@ -43,35 +47,55 @@ public class Game {
     private void playerMove() {
         Scanner scanner = new Scanner(System.in);
         choice = scanner.nextInt();
-        playerCheck();
+
+        switchPlayer();
+
         if (choice > 0 && choice < 10) {
             switch (choice) {
                 case 1:
-                    grid[2][0] = player;
+                    if (grid[2][0] == ' ') {
+                        grid[2][0] = player;
+                    }
                     break;
                 case 2:
-                    grid[2][1] = player;
+                    if (grid[2][1] == ' ') {
+                        grid[2][1] = player;
+                    }
                     break;
                 case 3:
-                    grid[2][2] = player;
+                    if (grid[2][2] == ' ') {
+                        grid[2][2] = player;
+                    }
                     break;
                 case 4:
-                    grid[1][0] = player;
+                    if (grid[1][0] == ' ') {
+                        grid[1][0] = player;
+                    }
                     break;
                 case 5:
-                    grid[1][1] = player;
+                    if (grid[1][1] == ' ') {
+                        grid[1][1] = player;
+                    }
                     break;
                 case 6:
-                    grid[1][2] = player;
+                    if (grid[1][2] == ' ') {
+                        grid[1][2] = player;
+                    }
                     break;
                 case 7:
-                    grid[0][0] = player;
+                    if (grid[0][0] == ' ') {
+                        grid[0][0] = player;
+                    }
                     break;
                 case 8:
-                    grid[0][1] = player;
+                    if (grid[0][1] == ' ') {
+                        grid[0][1] = player;
+                    }
                     break;
                 case 9:
-                    grid[0][2] = player;
+                    if (grid[0][2] == ' ') {
+                        grid[0][2] = player;
+                    }
                     break;
                 default:
                     break;
@@ -79,7 +103,7 @@ public class Game {
         }
     }
 
-    private void playerCheck() {
+    private void switchPlayer() {
         if (player == 'x') {
             player = 'o';
         } else {
