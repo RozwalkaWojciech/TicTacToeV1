42aa45fef9ab0ce361a471494c551f645c604c73
Wojciech Rozwałka <wojciechrozwalka@gmail.com>
2020-12-16 11:07:39 +0100

    fix playerMove switch

--- src/main/java/game/java/game/Game.java
+++ src/main/java/game/java/game/Game.java
@@ -47,26 +47,35 @@ public class Game {
             switch (choice) {
                 case 1:
                     grid[2][0] = player;
+                    break;
                 case 2:
                     grid[2][1] = player;
+                    break;
                 case 3:
                     grid[2][2] = player;
+                    break;
                 case 4:
                     grid[1][0] = player;
+                    break;
                 case 5:
                     grid[1][1] = player;
+                    break;
                 case 6:
                     grid[1][2] = player;
+                    break;
                 case 7:
                     grid[0][0] = player;
+                    break;
                 case 8:
                     grid[0][1] = player;
+                    break;
                 case 9:
                     grid[0][2] = player;
                     break;
+                default:
+                    break;
             }
         }
-
     }
 
     private void playerCheck() {
